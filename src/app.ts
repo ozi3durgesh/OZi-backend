099f610b6a1aaff0c9b5b0587331a80d626911cd
Ankit Gupta <ankit.gupta@ozi.in>
2025-09-08 17:32:18 +0530

    remove_dublicate

--- src/app.ts
+++ src/app.ts
@@ -37,7 +37,6 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Body parsing middleware
-app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
